56ecc0a7310d860fdb0537fc7d205472af6b2cb3
Ilya Bakhlin <gitlab@ilyabakhlin.com>
2021-01-03 15:01:37 +0100

    Adding the supported numbers.

---
+++ src/TypeScript/Objects/Numbers.ts
@@ -0,0 +1,37 @@
+import {Key} from "../Types/Key";
+
+/**
+ * @type Key[]
+ */
+export const Numbers: Key[] = [
+    {
+        code: "Digit0",
+    },
+    {
+        code: "Digit1",
+    },
+    {
+        code: "Digit2",
+    },
+    {
+        code: "Digit3",
+    },
+    {
+        code: "Digit4",
+    },
+    {
+        code: "Digit5",
+    },
+    {
+        code: "Digit6",
+    },
+    {
+        code: "Digit7",
+    },
+    {
+        code: "Digit8",
+    },
+    {
+        code: "Digit9",
+    },
+];
