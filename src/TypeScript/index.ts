9b6bbafee1656631ab7bb0c775b87d9289e0c886
Ilya Bakhlin <gitlab@ilyabakhlin.com>
2021-01-11 00:21:40 +0100

    Implementing the Translitr class in the frontend.

---
+++ src/TypeScript/index.ts
@@ -0,0 +1,62 @@
+import {Translitr} from "./Classes/Translitr";
+import {Translitr as TranslitrInterface} from "./Interfaces/Translitr";
+
+/**
+ * @param {string} query
+ * @return {Element}
+ */
+function getElement(query: string): Element
+{
+    const element: Element | null = window.document.querySelector(query);
+    if (element instanceof Element) {
+        return element;
+    } else {
+        throw new Error(); // TODO: Define an error message.
+    }
+}
+
+/**
+ * @param {string} query
+ * @return {HTMLSelectElement}
+ */
+function getElementSelect(query: string): HTMLSelectElement
+{
+    const select: Element = getElement(query);
+    if (select instanceof HTMLSelectElement) {
+        return select;
+    } else {
+        throw new Error(); // TODO: Define an error message.
+    }
+}
+
+/**
+ * @param {string} query
+ * @return {HTMLTextAreaElement}
+ */
+function getElementTextArea(query: string): HTMLTextAreaElement
+{
+    const textArea: Element = getElement(query);
+    if (textArea instanceof HTMLTextAreaElement) {
+        return textArea;
+    } else {
+        throw new Error(); // TODO: Define an error message.
+    }
+}
+
+window.addEventListener("load", (): void => {
+    const content: HTMLTextAreaElement = getElementTextArea("textarea#content");
+    const layouts: HTMLSelectElement = getElementSelect("select#layouts");
+    const translitr: TranslitrInterface = new Translitr(layouts.options[layouts.selectedIndex].value);
+
+    layouts.addEventListener("change", (): void => {
+        translitr.layout = layouts.options[layouts.selectedIndex].value;
+    });
+
+    content.addEventListener("keydown", (event: KeyboardEvent): void => {
+        window.console.log(event);
+    });
+
+    content.addEventListener("keyup", (event: KeyboardEvent): void => {
+        window.console.log(event);
+    });
+});
